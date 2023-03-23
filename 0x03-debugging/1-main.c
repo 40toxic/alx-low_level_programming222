317bcdd9f57a836f19922851d382dad0e3c4edec
40toxic <93138535+40toxic@users.noreply.github.com>
2023-03-23 14:22:10 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-/**
-* main - causes an infinite loop
-* Return: 0
-*/
-int main(void)
-{
-int i;
-printf("Infinite loop incoming :(\n");
-i = 0;
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
+int main(void) {
+    int count = 0;
+
+    while (1) {  // infinite loop
+        printf("Loop count: %d\n", count);
+        count++;
+    }
+
+    return 0;
 }
